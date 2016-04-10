2492686104c1fc2ff9f5bf567d2a12268b825191
João Simões <nogravity00@outlook.com>
2016-04-10 15:06:30 +0100

    Added EF mapping

---
+++ SimplePersistence.Example/SimplePersistence.Example.UoW.EF/Mapping/ExampleContext.cs
@@ -0,0 +1,60 @@
+﻿using System.Data.Entity;
+using SimplePersistence.Model.EF.Fluent;
+
+namespace SimplePersistence.Example.UoW.EF.Mapping
+{
+    public class ExampleContext : DbContext
+    {
+        public ExampleContext() : this("name=Example")
+        {
+
+        }
+
+        public ExampleContext(string nameOrConnectionString) : base(nameOrConnectionString)
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            #region Logging
+
+            modelBuilder.Entity<Models.Logging.Application>(cfg =>
+            {
+                cfg.HasKey(e => e.Id)
+                    .Property(e => e.Id).HasMaxLength(128);
+                cfg.Property(e => e.Description).IsRequired().HasMaxLength(512);
+
+                cfg.MapCreatedMeta().MapUpdatedMeta().MapDeletedMeta().MapByteArrayVersion();
+            }, "Applications", "Logging");
+
+            modelBuilder.Entity<Models.Logging.Level>(cfg =>
+            {
+                cfg.HasKey(e => e.Id)
+                    .Property(e => e.Id).HasMaxLength(128);
+                cfg.Property(e => e.Description).IsRequired().HasMaxLength(512);
+
+                cfg.MapCreatedMeta().MapUpdatedMeta().MapDeletedMeta().MapByteArrayVersion();
+            }, "Levels", "Logging");
+
+            modelBuilder.Entity<Models.Logging.Log>(cfg =>
+            {
+                cfg.HasIdentityKeyAsLong();
+                cfg.HasRequired(e => e.Level).WithMany(e => e.Logs).Map(m => m.MapKey("Level"));
+                cfg.Property(e => e.Logger).IsRequired().HasMaxLength(256);
+                cfg.Property(e => e.Message).IsRequired();
+                cfg.Property(e => e.Exception).IsOptional();
+                cfg.HasRequired(e => e.Application).WithMany(e => e.Logs).Map(m => m.MapKey("Application"));
+                cfg.Property(e => e.Thread).IsOptional().HasMaxLength(128);
+                cfg.Property(e => e.MachineName).IsRequired().HasMaxLength(128).AddIndex();
+                cfg.Property(e => e.AssemblyVersion).IsRequired().HasMaxLength(32).AddIndex();
+
+                cfg.MapCreatedMeta(onNeedsIndex: true);
+            }, "Logs", "Logging");
+
+            #endregion
+        }
+    }
+}
